8b5e670277c7390d8231a5b97c618f22c4d30fd0
Raunak Raj <71929976+bajrangCoder@users.noreply.github.com>
2025-04-26 20:30:48 +0530

    feat: improve highlighting and fix known issues #11 #1

--- languages/scss/highlights.scm
+++ languages/scss/highlights.scm
@@ -1,70 +1,11 @@
-[
-  "@at-root"
-  "@debug"
-  "@error"
-  "@extend"
-  "@forward"
-  "@mixin"
-  "@use"
-  "@warn"
-] @keyword
-
-"@function" @keyword.function
-
-"@return" @keyword.return
-
-"@include" @keyword.import
-
-"@import" @keyword.import
-
-"@keyframes" @keyword.import
-
-[
-  "@while"
-  "@each"
-  "@for"
-  "from"
-  "through"
-  "in"
-] @keyword.repeat
-
-(js_comment) @comment
-
-(comment) @comment
-
-(function_name) @function
-
-(variable) @variable
-
-(mixin_statement
-  name: (identifier) @function)
-
-(mixin_statement
-  (parameters
-    (parameter) @variable.parameter))
-
-(function_statement
-  name: (identifier) @function)
-
-(function_statement
-  (parameters
-    (parameter) @variable.parameter))
-
-(include_statement
-  (identifier) @function)
-
-(argument) @variable.parameter
-
-(arguments
-  (variable) @variable.parameter)
-
-(comment) @comment
+[(comment) (single_line_comment)] @comment
 
 [
   (tag_name)
-  (nesting_selector)
   (universal_selector)
 ] @tag
+(attribute_selector (plain_value) @string)
+(nesting_selector) @variable.other.member
 
 [
   "~"
@@ -88,18 +29,25 @@
 ] @operator
 
 (attribute_selector (plain_value) @string)
-
-(attribute_name) @attribute
 (pseudo_element_selector (tag_name) @attribute)
 (pseudo_class_selector (class_name) @attribute)
+
+(variable_name) @variable.other.member
+(variable_value) @variable.other.member
+(argument_name) @variable.parameter
 
 [
   (class_name)
   (id_name)
   (namespace_name)
-  (property_name)
   (feature_name)
+  (identifier)
+  (property_name)
 ] @property
+
+(attribute_name) @attribute
+
+(function_name) @function
 
 (
   [
@@ -110,12 +58,47 @@
 )
 
 [
-  "@media"
-  "@import"
-  "@charset"
-  "@namespace"
-  "@supports"
-  "@keyframes"
+    "@at-root"
+    "@charset"
+    "@debug"
+    "@error"
+    "@extend"
+    "@keyframes"
+    "@media"
+    "@mixin"
+    "@supports"
+    "@warn"
+] @constant.builtin
+
+"@function" @function.method
+
+"@namespace" @namespace
+
+"@return" @keyword.control.return
+
+[
+    "@else"
+    "@if"
+] @keyword.control.conditional
+
+[
+  "@while"
+  "@each"
+  "@for"
+  "through"
+  "in"
+  "from"
+  "if"
+] @keyword.repeat
+
+[
+    "@forward"
+    "@import"
+    "@include"
+    "@use"
+] @keyword.control.import
+
+[
   (at_keyword)
   (to)
   (from)
@@ -129,10 +112,22 @@
   (integer_value)
   (float_value)
 ] @number
-
 (unit) @type
 
 [
   ","
   ":"
+  "."
+  "::"
+  ";"
+  "#"
 ] @punctuation.delimiter
+
+[
+  "{"
+  ")"
+  "("
+  "}"
+  "["
+  "]"
+] @punctuation.bracket
